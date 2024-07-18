f669dfbe867f2d1bb49a582cd1a5f2833b623718
majid ali <majidalijkhio@gmail.com>
2024-07-18 21:16:11 +0500

    created dynamic layout

--- src/router.tsx
+++ src/router.tsx
@@ -1,13 +1,18 @@
 import { createBrowserRouter } from "react-router-dom";
 import LoginPage from "./pages/Login";
-import HomePage from "./pages/Home";
+import Home from "./pages/Home";
 import RegisterPage from "./pages/RegisterPage";
+import DashboardLayout from "./layout/DashboardLayout";
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <HomePage/>,
+    path: "/dashboard",
+    element: <DashboardLayout/>,
+    children: [
+      { path: "home", element: <Home /> },
+    ],
   },
+
   {
     path: "/login", 
     element: <LoginPage />,
